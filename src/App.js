15f8c219b948c4db550294b8f72fe42641fe780a
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-05-30 10:16:43 +0300

    5.1: blogilistan frontend, step1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +15,64 @@ const App = () => {
     )  
   }, [])
 
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService
+      .login({ username, password })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    console.log('Logged in with user ', user)
+
+    } catch (error) {
+      console.log('käyttäjätunnus tai salasana virheellinen')
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+      <h2>log into application</h2>
+      
+      <form onSubmit={handleLogin}>
+        <div>
+          käyttäjätunnus
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          salasana
+            <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">kirjaudu</button>
+      </form>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <p>
+        {user.name} logged in
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
