a7aa0683ef40d454a0da764de88a4d9d0a5a9d04
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-06-02 02:02:05 +0300

    5.8*: blogilistan frontend, step8

--- src/components/Blogit.js
+++ src/components/Blogit.js
@@ -33,9 +33,11 @@ const Blogit = () => {
   return (
     <div>
       {blogForm()}
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
     </div>
   )
 }
