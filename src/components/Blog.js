74d1425be29742eeb91046324e1b5ed4886fe02f
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-06-01 20:20:36 +0300

    5.6* blogilistan frontend, step6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,56 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false)
+
+  const handleClick = (event) => {
+    setShowAll(!showAll)
+    console.log(`showAll: ${showAll}`)
+  }
+  
+  const showBlog = () => {
+    return (
+      <div>
+        <p>
+          {blog.title} {blog.author}
+        </p>
+      </div>)
+  }
+
+  const showBlogDetails = () => {
+    console.log(blog)
+    return (
+      <div>
+        <p>
+          {blog.title} {blog.author}
+        </p>
+        <a href={blog.url}  rel="noopener noreferrer" target="_blank">
+          {blog.url}
+        </a>
+        <p>
+          {blog.likes} tykkäystä
+          <button>like</button>
+        </p>
+        <p>
+          Added by {blog.user[0].name}
+        </p>
+
+      </div>
+    )
+  }
+
+  return (
+    <div className="blog">
+      <div onClick={handleClick}>
+        {showAll ?
+          showBlogDetails() :
+          showBlog()
+        }
+      </div>
+    </div>
+  )
+  
+}
 
 export default Blog
